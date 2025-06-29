a0b39caaf83c7016009ae8ef0c2a07c9de30cda3
Abhigy012 <abhygy454@gmail.com>
2025-06-29 17:21:33 +0530

    m

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
 
         <div>
-          <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <img className='w-40 mb-5' src={assets.logo} alt="" />
+          <p className='w-full leading-6 text-gray-600 md:w-2/3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
 
         <div>
-          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <p className='mb-5 text-xl font-medium'>COMPANY</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>Home</li>
             <li>About us</li>
@@ -22,7 +22,7 @@ const Footer = () => {
         </div>
 
         <div>
-          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <p className='mb-5 text-xl font-medium'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+1-212-456-7890</li>
             <li>greatstackdev@gmail.com</li>
